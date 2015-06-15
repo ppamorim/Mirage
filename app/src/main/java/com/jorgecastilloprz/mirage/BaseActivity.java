fdcef7d33cbbd8b18a7947ad051d610cbc77d8fe
jorgecastilloprz <jorge.castillo.prz@gmail.com>
2015-06-15 18:57:40 +0200

    Added intro video.

---
+++ app/src/main/java/com/jorgecastilloprz/mirage/BaseActivity.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2014 Jorge Castillo Pérez
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.jorgecastilloprz.mirage;
+
+import android.support.v7.app.AppCompatActivity;
+import butterknife.ButterKnife;
+
+/**
+ * BaseActivity will be extended by every activity in the app, and it hides
+ * common logic for concrete activities, like initial view injections
+ *
+ * Created by jorge on 10/01/15.
+ */
+public abstract class BaseActivity extends AppCompatActivity {
+
+  protected void injectViews() {
+    ButterKnife.inject(this);
+  }
+}
--- app/src/main/java/com/jorgecastilloprz/mirage/SignUpActivity.java
+++ app/src/main/java/com/jorgecastilloprz/mirage/SignUpActivity.java
@@ -1,12 +1,95 @@
 package com.jorgecastilloprz.mirage;
 
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.FrameLayout;
+import butterknife.InjectView;
+import com.jorgecastilloprz.mirage.components.TextureVideoView;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
-public class SignUpActivity extends AppCompatActivity {
+public class SignUpActivity extends BaseActivity {
+
+  @InjectView(R.id.textureView) TextureVideoView textureView;
+  @InjectView(R.id.root) FrameLayout root;
+
+  private MediaPlayer mMediaPlayer;
+  private View mDecorView;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_signup);
+    injectViews();
+    mDecorView = getWindow().getDecorView();
+    setupGestureDetector();
+    initBackgroundVideo();
+  }
+
+  @Override protected void attachBaseContext(Context newBase) {
+    super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+  }
+
+  @Override public void onWindowFocusChanged(boolean hasFocus) {
+    super.onWindowFocusChanged(hasFocus);
+    if (hasFocus && Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+      mDecorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+          | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+          | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+          | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+          | View.SYSTEM_UI_FLAG_FULLSCREEN
+          | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+    }
+  }
+
+  private void setupGestureDetector() {
+    final GestureDetector clickDetector =
+        new GestureDetector(this, new GestureDetector.SimpleOnGestureListener() {
+          @Override public boolean onSingleTapUp(MotionEvent e) {
+            showSystemUI();
+            return true;
+          }
+        });
+
+    root.setOnTouchListener(new View.OnTouchListener() {
+      @Override public boolean onTouch(View view, MotionEvent motionEvent) {
+        return clickDetector.onTouchEvent(motionEvent);
+      }
+    });
+  }
+
+  @TargetApi(Build.VERSION_CODES.KITKAT) private void showSystemUI() {
+    mDecorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+  }
+
+  private void initBackgroundVideo() {
+    textureView.setScaleType(TextureVideoView.ScaleType.CENTER_CROP);
+    textureView.setDataSource(this,
+        Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.mirage));
+    textureView.setLooping(true);
+    textureView.setListener(new TextureVideoView.MediaPlayerListener() {
+      @Override public void onVideoPrepared() {
+        textureView.play();
+      }
+
+      @Override public void onVideoEnd() {
+      }
+    });
+  }
+
+  @Override protected void onDestroy() {
+    super.onDestroy();
+    if (mMediaPlayer != null) {
+      mMediaPlayer.stop();
+      mMediaPlayer.release();
+      mMediaPlayer = null;
+    }
   }
 }
