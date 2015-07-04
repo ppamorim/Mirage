6c2bcff610abfa7e1375366a7feef8fb6459805f
jorgecastilloprz <jorge.castillo.prz@gmail.com>
2015-07-04 11:41:49 +0200

    Added coordinator animations to tablayout and fab.

--- app/src/main/java/com/jorgecastilloprz/mirage/ui/components/SignInButtonBox.java
+++ app/src/main/java/com/jorgecastilloprz/mirage/ui/components/SignInButtonBox.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.jorgecastilloprz.mirage.components;
+package com.jorgecastilloprz.mirage.ui.components;
 
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
--- app/src/main/java/com/jorgecastilloprz/mirage/ui/components/TextureVideoView.java
+++ app/src/main/java/com/jorgecastilloprz/mirage/ui/components/TextureVideoView.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.jorgecastilloprz.mirage.components;
+package com.jorgecastilloprz.mirage.ui.components;
 
 /**
  * @author Jorge Castillo Pérez
