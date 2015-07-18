7176868e285d217e901c8940475fe341fa1cf210
Jorge Castillo <jorge.castillo.prz@gmail.com>
2015-07-18 17:08:35 +0200

    Added MirageSigninActivityTest.

--- app/src/androidTest/java/com/jorgecastilloprz/mirage/MirageSignInActivityTest.java
+++ app/src/androidTest/java/com/jorgecastilloprz/mirage/MirageSignInActivityTest.java
@@ -3,23 +3,26 @@ package com.jorgecastilloprz.mirage;
 import android.support.test.espresso.assertion.ViewAssertions;
 import android.support.test.runner.AndroidJUnit4;
 import com.jorgecastilloprz.mirage.ui.activity.MainActivity;
+import com.jorgecastilloprz.mirage.ui.activity.MirageSignInActivity;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 /**
  * @author jorge
  * @since 18/07/15
  */
-@RunWith(AndroidJUnit4.class) public class MainActivityTest {
+@RunWith(AndroidJUnit4.class) public class MirageSignInActivityTest {
 
-  @Rule public final ActivityRule<MainActivity> main = new ActivityRule<>(MainActivity.class);
+  @Rule public final ActivityRule<MirageSignInActivity> main =
+      new ActivityRule<>(MirageSignInActivity.class);
 
-  @Test public void shouldBeAbleToLaunchMainScreen() {
-    onView(withText("Hello")).check(ViewAssertions.matches(isDisplayed()));
+  @Test public void videoShouldBePlaying() {
+    onView(withId(R.id.textureView)).check(ViewAssertions.matches(isDisplayed()));
   }
 }
